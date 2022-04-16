d11e310b3702e974ae4e1f6f4da90706125350de
Bogdan316 <bogdan.pirtica@student.upt.ro>
2022-04-16 13:25:52 +0300

    IA-10 lock app orientation, change primary swatch to custom color, disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'package:internship_app_fis/base_widgets/theme_color.dart';
 import 'package:internship_app_fis/pages/login_page.dart';
 
 void main() {
@@ -11,10 +13,17 @@ class InternshipApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Internship App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: createMaterialColor(
+          const Color(0xFF01135d),
+        ),
       ),
       home: const LoginPage(),
     );
