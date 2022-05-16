63c6c90ea73852fa701338b67273059502de1a26
Scortescu1 <radu.scortescu@student.upt.ro>
2022-05-16 20:19:30 +0300

    IA-29 Update the database tables with new data: delete useless folders

--- secund/android/app/src/main/kotlin/upt/fis/internship_app_fis/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package upt.fis.internship_app_fis
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- secund/lib/exceptions/user_already_exists.dart
+++
@@ -1,6 +0,0 @@
-class UserAlreadyExistsException implements Exception {
-  @override
-  String toString() {
-    return 'The username already exists.';
-  }
-}
